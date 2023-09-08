170f0ab28fce6985a4e88b585ba546d132d3d1b2
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-08 20:55:08 +0600

    style: css/style issue fix

--- src/components/UI/TicTacToe/TicTacToe.js
+++ src/components/UI/TicTacToe/TicTacToe.js
@@ -3,42 +3,42 @@ import React from "react";
 const TicTacToe = () => {
   return (
     <div>
-      <div className="game-container flex flex-col justify-center items-center">
-        <div className="turn">
-          <div className="your-turn">
+      <div class="game-container flex flex-col justify-center items-center">
+        <div class="turn">
+          <div class="your-turn">
             <h1>Your Turn (X) : </h1>
           </div>
-          <div className="my-turn">
+          <div class="my-turn">
             <h1>My Turn (O) </h1>
           </div>
         </div>
-        <div className="game row row-cols-3 m-0">
-          <div className="col border-bt border-rt">
-            <button className="my-btn"></button>
+        <div class="game grid grid-cols-3 gap-0">
+          <div class="col border-b border-r">
+            <button class="my-btn"></button>
           </div>
-          <div className="col border-bt border-rt">
-            <button className="my-btn"></button>
+          <div class="col border-b border-r">
+            <button class="my-btn"></button>
           </div>
-          <div className="col border-bt">
-            <button className="my-btn"></button>
+          <div class="col border-b">
+            <button class="my-btn"></button>
           </div>
-          <div className="col border-bt border-rt">
-            <button className="my-btn"></button>
+          <div class="col border-b border-r">
+            <button class="my-btn"></button>
           </div>
-          <div className="col border-bt border-rt">
-            <button className="my-btn"></button>
+          <div class="col border-b border-r">
+            <button class="my-btn"></button>
           </div>
-          <div className="col border-bt">
-            <button className="my-btn"></button>
+          <div class="col border-b">
+            <button class="my-btn"></button>
           </div>
-          <div className="col border-rt">
-            <button className="my-btn"></button>
+          <div class="col border-r">
+            <button class="my-btn"></button>
           </div>
-          <div className="col border-rt">
-            <button className="my-btn"></button>
+          <div class="col border-r">
+            <button class="my-btn"></button>
           </div>
-          <div className="col">
-            <button className="my-btn"></button>
+          <div class="col">
+            <button class="my-btn"></button>
           </div>
         </div>
       </div>
